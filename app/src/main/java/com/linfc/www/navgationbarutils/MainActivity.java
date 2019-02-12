55b6cd59e3724d93cc7d0c3036683c9060093ec0
fengchaolin <linfengchao@taqu.cn>
2019-02-12 14:15:51 +0800

    代码优化调整

--- app/src/main/java/com/linfc/www/navgationbarutils/MainActivity.java
+++ app/src/main/java/com/linfc/www/navgationbarutils/MainActivity.java
@@ -15,11 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView tvTest = findViewById(R.id.tvTest);
+
+        final TextView tvInfo = findViewById(R.id.tvInfo);
+
         tvTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvTest.setText(NavgationbarUtils.navgationbarIsOpen(MainActivity.this)+"");
-                Toast.makeText(MainActivity.this, NavgationbarUtils.getNavigationBarHeight(MainActivity.this)+"", Toast.LENGTH_SHORT).show();
+                StringBuilder str = new StringBuilder();
+                if (NavgationbarUtils.navgationbarIsOpen(MainActivity.this)){
+                    str.append("导航栏开启了");
+                }else {
+                    str.append("导航栏关闭了");
+                }
+                tvTest.setText(str.toString());
+                tvInfo.setText("动态获取到的导航栏高度:"+NavgationbarUtils.getNavigationBarHeight(MainActivity.this));
+
             }
         });
     }
