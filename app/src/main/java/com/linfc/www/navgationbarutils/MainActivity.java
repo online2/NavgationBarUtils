d9b4412cfeba8f6101fe7485f630e17b9071fd76
fengchaolin <linfengchao@taqu.cn>
2019-02-12 14:08:12 +0800

    代码优化调整

--- app/src/main/java/com/linfc/www/navgationbarutils/MainActivity.java
+++ app/src/main/java/com/linfc/www/navgationbarutils/MainActivity.java
@@ -2,6 +2,11 @@ package com.linfc.www.navgationbarutils;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.linfc.www.navgationbarutils.utils.NavgationbarUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView tvTest = findViewById(R.id.tvTest);
+        tvTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvTest.setText(NavgationbarUtils.navgationbarIsOpen(MainActivity.this)+"");
+                Toast.makeText(MainActivity.this, NavgationbarUtils.getNavigationBarHeight(MainActivity.this)+"", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/linfc/www/navgationbarutils/utils/NavgationbarUtils.java
+++ app/src/main/java/com/linfc/www/navgationbarutils/utils/NavgationbarUtils.java
@@ -1,9 +1,11 @@
 package com.linfc.www.navgationbarutils.utils;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.FitWindowsLinearLayout;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -15,66 +17,16 @@ import android.widget.LinearLayout;
  */
 public class NavgationbarUtils {
 
-    private final Window mWindow;
-    private final View mContentView;
-    private ViewGroup mOnlyLinearLayout;
-    private int navigationBarHeight = 0;
-
-    private NavgationbarUtils(Activity activity) {
-        mWindow = activity.getWindow();
-        mContentView = activity.findViewById(android.R.id.content);
-        findRootLinearLayout();
-    }
-
-    private NavgationbarUtils(Fragment fragment) {
-        mWindow = fragment.getActivity().getWindow();
-        mContentView = fragment.getView();
-        findRootLinearLayout();
-    }
-
-
-    private NavgationbarUtils(Dialog dialog) {
-        mWindow = dialog.getWindow();
-        mContentView = dialog.findViewById(android.R.id.content);
-        findRootLinearLayout();
-    }
-
-    /**
-     * 从R.id.content从上遍历，拿到 DecorView 下的唯一子布局LinearLayout
-     * 获取对应的bottomMargin 即可得到对应导航栏的高度，0为关闭了或没有导航栏
-     */
-    private void findRootLinearLayout() {
-        try {
-            ViewGroup decorView = (ViewGroup) mWindow.getDecorView();
-            if (mContentView != null) {
-                View tempView = mContentView;
-                while (tempView.getParent() != decorView) {
-                    ViewGroup parent = (ViewGroup) tempView.getParent();
-                    if (parent instanceof LinearLayout) {
-                        //如果style设置了不带toolbar,mContentView上层布局会由原来的 ActionBarOverlayLayout->FitWindowsLinearLayout)
-                        if (parent instanceof FitWindowsLinearLayout) {
-                            tempView = parent;
-                            continue;
-                        } else {
-                            mOnlyLinearLayout = parent;
-                            break;
-                        }
-                    } else {
-                        tempView = parent;
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
      * @return false 关闭了NavgationBar ,true 开启了
      */
-    public boolean navgationbarIsOpen() {
-        if (mOnlyLinearLayout != null) {
-            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) mOnlyLinearLayout.getLayoutParams();
+    public static boolean navgationbarIsOpen(Context context) {
+        ViewGroup rootLinearLayout = findRootLinearLayout(context);
+        int navigationBarHeight = 0;
+
+        if (rootLinearLayout != null) {
+            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) rootLinearLayout.getLayoutParams();
             navigationBarHeight = layoutParams.bottomMargin;
         }
         if (navigationBarHeight == 0) {
@@ -84,11 +36,96 @@ public class NavgationbarUtils {
         }
     }
 
-    public int getNavigationBarHeight() {
-        if (mOnlyLinearLayout != null) {
-            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) mOnlyLinearLayout.getLayoutParams();
+    /**
+     * 导航栏高度，关闭的时候返回0,开启时返回对应值
+     *
+     * @param context
+     * @return
+     */
+    public static int getNavigationBarHeight(Context context) {
+        ViewGroup rootLinearLayout = findRootLinearLayout(context);
+        int navigationBarHeight = 0;
+        if (rootLinearLayout != null) {
+            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) rootLinearLayout.getLayoutParams();
             navigationBarHeight = layoutParams.bottomMargin;
         }
         return navigationBarHeight;
+    }
+
+    /**
+     * 从R.id.content从上遍历，拿到 DecorView 下的唯一子布局LinearLayout
+     * 获取对应的bottomMargin 即可得到对应导航栏的高度，0为关闭了或没有导航栏
+     */
+    private static ViewGroup findRootLinearLayout(Context context) {
+        ViewGroup onlyLinearLayout = null;
+        try {
+            Window window = getWindow(context);
+            if (window != null) {
+                ViewGroup decorView = (ViewGroup) getWindow(context).getDecorView();
+                Activity activity = getActivity(context);
+                View contentView = activity.findViewById(android.R.id.content);
+                if (contentView != null) {
+                    View tempView = contentView;
+                    while (tempView.getParent() != decorView) {
+                        ViewGroup parent = (ViewGroup) tempView.getParent();
+                        if (parent instanceof LinearLayout) {
+                            //如果style设置了不带toolbar,mContentView上层布局会由原来的 ActionBarOverlayLayout->FitWindowsLinearLayout)
+                            if (parent instanceof FitWindowsLinearLayout) {
+                                tempView = parent;
+                                continue;
+                            } else {
+                                onlyLinearLayout = parent;
+                                break;
+                            }
+                        } else {
+                            tempView = parent;
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return onlyLinearLayout;
+    }
+
+
+    private static Window getWindow(Context context) {
+        if (getAppCompActivity(context) != null) {
+            return getAppCompActivity(context).getWindow();
+        } else {
+            return scanForActivity(context).getWindow();
+        }
+    }
+
+    private static Activity getActivity(Context context) {
+        if (getAppCompActivity(context) != null) {
+            return getAppCompActivity(context);
+        } else {
+            return scanForActivity(context);
+        }
+    }
+
+
+    private static AppCompatActivity getAppCompActivity(Context context) {
+        if (context == null) return null;
+        if (context instanceof AppCompatActivity) {
+            return (AppCompatActivity) context;
+        } else if (context instanceof ContextThemeWrapper) {
+            return getAppCompActivity(((ContextThemeWrapper) context).getBaseContext());
+        }
+        return null;
+    }
+
+    private static Activity scanForActivity(Context context) {
+        if (context == null) return null;
+
+        if (context instanceof Activity) {
+            return (Activity) context;
+        } else if (context instanceof ContextWrapper) {
+            return scanForActivity(((ContextWrapper) context).getBaseContext());
+        }
+        return null;
     }
 }
